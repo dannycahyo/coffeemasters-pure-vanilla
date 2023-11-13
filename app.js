cf22d27541754da0fe7569099476ad952e6d0d4b
dannydcahyono <dannydwicahyono@gmail.com>
2023-11-13 08:56:06 +0700

    feat: serve all of the component on the root file

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-import { API } from "./services/API";
-import { Router } from "./services/Router";
+import { Router } from "./services/Router.js";
 import Store from "./services/Store.js";
-import { loadMenuData } from "./services/Menu";
+import { loadMenuData } from "./services/Menu.js";
+
+import { MenuPage } from "./components/MenuPage.js";
+import { DetailsPage } from "./components/DetailsPage.js";
+import { OrderPage } from "./components/OrderPage.js";
+import { ProductItem } from "./components/ProductItem.js";
+import { CartItem } from "./components/CartItem.js";
 
 window.app = {};
 app.store = Store;
 app.router = Router;
+
+function renderComponents() {
+  customElements.define("menu-page", MenuPage);
+  customElements.define("details-page", DetailsPage);
+  customElements.define("order-page", OrderPage);
+  customElements.define("product-item", ProductItem);
+  customElements.define("cart-item", CartItem);
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   loadMenuData();
@@ -18,3 +31,5 @@ window.addEventListener("appcartchange", () => {
   badge.textContent = qty;
   badge.hidden = qty == 0;
 });
+
+renderComponents();
